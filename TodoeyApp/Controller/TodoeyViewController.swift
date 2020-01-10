88fa78b4989fa061efede98dabab3be6325c7193
Zizo <zizoadel26@gmail.com>
2020-01-10 03:42:43 -0800

    save data using core data

--- TodoeyApp/Controller/TodoeyViewController.swift
+++ TodoeyApp/Controller/TodoeyViewController.swift
@@ -7,33 +7,95 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoeyViewController: UITableViewController {
 
-    var itemArray = ["Zizo", "Ali", "Hosam"]
+    // MARK: - Variables
+    var itemArray: [Item] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    // MARK: - Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        addCustomCell()
+        loadItems()
+    }
+
+    // MARK: - Actions
+    @IBAction func addNewItemBarButtonAction(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
+            let newItem = textField.text!
+            let item = Item(context: self.context)
+            item.title = newItem
+            item.done = false
+            self.itemArray.append(item)
+            self.saveNewItem()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addTextField { (alertTF) in
+            alertTF.placeholder = "Enter the name of item"
+            textField = alertTF
+        }
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - Helper methods
+    /*** add custom cell to the table view ***/
+    func addCustomCell() {
         let nibCell = UINib(nibName: "TodoeyCell", bundle: nil)
         tableView.register(nibCell, forCellReuseIdentifier: "Cell")
     }
+    
+    /*** save new item the itemArray ***/
+    func saveNewItem() {
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+        self.tableView.reloadData()
+    }
+    
+    /*** load all items ***/
+    func loadItems() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
 
-    // MARK: - cofigure table view
+// MARK: - cofigure table view
+extension TodoeyViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TodoeyCell
-        cell.titleLbl.text = itemArray[indexPath.row]
-        
+        let currentItem = itemArray[indexPath.row]
+        cell.titleLbl.text = currentItem.title
+        cell.accessoryType = currentItem.done ? .checkmark : .none
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveNewItem()
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60
     }
-
 }
-
